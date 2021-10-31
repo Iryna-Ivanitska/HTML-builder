72272f8f872c829b1bed9f37cd4170d5a8b1d506
Ityna Ivanytska <ira.pechenitsya@gmal.com>
2021-11-01 00:35:28 +0300

    feat: add second task write to file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,1 +1,21 @@
-const process = require('process');
+const { stdin: input, stdout: output } = require("process");
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+
+const stream = fs.createWriteStream(path.join(__dirname, "text.txt"));
+
+const rl = readline.createInterface({ input, output });
+console.log("Enter your TODO list");
+
+rl.on("line", (input) => {
+  if (input === "exit") {
+    rl.close();
+  } else { 
+    stream.write(`${input}\n`);
+  }
+});
+
+rl.on("close", () => {
+  console.log("Don't forget to do your tasks");
+});
