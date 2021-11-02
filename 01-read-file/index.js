bc9496e63d70645e1ceb10859aa01b55888a7452
Ityna Ivanytska <ira.pechenitsya@gmal.com>
2021-11-03 01:49:28 +0300

    feat: added task 4 - copy files in folder

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,13 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-// var stream = new fs.ReadStream(path.join(__dirname, "/text.txt"), {encoding: "utf-8" });
-
-// stream.on('readable', function(){
-//     var data = stream.read();
-//     console.log(data);
-// });
-
 let stream = fs.createReadStream(path.join(__dirname, "/text.txt"));
 
 stream.on("data", (data) => console.log(data.toString()));
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+const { copyFile } = require("fs");
+
+function errorThrow(err) {
+    if (err) throw err;
+}
+
+(function copyDir() {
+  const newDir = path.join(__dirname, "files-copy");
+  const oldDir = path.join(__dirname, "files");
+  fs.mkdir(newDir, { recursive: true }, errorThrow);
+
+  fs.readdir(oldDir, function (err, files) {
+    if (err) throw err;
+    for (const file of files) {
+      copyFile(path.join(oldDir, file), path.join(newDir, file), errorThrow);
+    }
+  });
+})();
 
