c4df7c8f720f8c0c9df0b321ae95349e6954db76
Ityna Ivanytska <ira.pechenitsya@gmal.com>
2021-11-03 02:19:45 +0300

    fix: task 2 - write in the file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,18 +3,24 @@ const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
 
+console.log("Enter your TODO list");
 const stream = fs.createWriteStream(path.join(__dirname, "text.txt"));
 
 const rl = readline.createInterface({ input, output });
-console.log("Enter your TODO list");
 
 rl.on("line", (input) => {
   if (input === "exit") {
     rl.close();
   } else { 
-    stream.write(`${input}\n`);
+    // stream.write(`${input}\n`);
+    // stream.end();
+    fs.appendFile(path.join(__dirname, "text.txt"), `${input}\n`, (err) => {
+      if (err) throw err;
+    })
   }
 });
+
+// stdin.on('data', input => stream.write(`${input}\n`));
 
 rl.on("close", () => {
   console.log("Don't forget to do your tasks");
